a08dc58b830de2805944d7d294154386262f80f5
kiril <kirillsiskinskij@gmail.com>
2026-02-04 01:09:04 +0300

    Этап 2. Разбиваем проект на микросервисы

--- core/event-service/src/main/java/ru/practicum/event/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/event/EventServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "ru.practicum")
 @EntityScan(basePackages = "ru.practicum")
+@EnableJpaRepositories(basePackages = "ru.practicum")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.practicum.client")
 public class EventServiceApplication {
