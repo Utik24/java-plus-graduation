a1b7a79a44fc7b62134049edb595f6d737824dad
kiril <kirillsiskinskij@gmail.com>
2026-02-04 10:07:53 +0300

    Этап 2. Разбиваем проект на микросервисы

--- core/common/src/main/java/ru/practicum/exception/CreateConditionException.java
+++ core/common/src/main/java/ru/practicum/exception/CreateConditionException.java
@@ -1,5 +1,8 @@
 package ru.practicum.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.CONFLICT)
 public class CreateConditionException extends RuntimeException {
     public CreateConditionException(String mess) {
         super(mess);
